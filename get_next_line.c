1e4f3891df62be748d41805da847b68387ad2e05
Leandr0Duar7e <lean_duart@hotmail.com>
2021-11-29 16:18:22 +0000

    Almostttt
    Falta resolver o fim do documento

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: leolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 18:33:21 by leolivei          #+#    #+#             */
-/*   Updated: 2021/11/23 15:07:34 by leolivei         ###   ########.fr       */
+/*   Updated: 2021/11/29 16:16:37 by leolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-static char	*makekeep(char **fdlst, char *rline, int fd)
+static char	*makekeep(char **fdlst, char *rline, int fd, int nrr)
 {
 	char	*line;//variavel a retornar
 	char	*rest;//guardar o pos /n e colocar no keep
@@ -22,16 +22,16 @@ static char	*makekeep(char **fdlst, char *rline, int fd)
 	if (rline)
 	{
 		if (!fdlst[fd])
-			fdlst[fd] = "";
+			fdlst[fd] = malloc(sizeof(char) * BUFFER_SIZE);
 		fdlst[fd] = ft_strjoin(fdlst[fd], rline);
 	}
-	printf("if1\n");
 	i = 0;
 	while (fdlst[fd][i])
 	{
+		//printf("%c\n", fdlst[fd][i]);
 		if (fdlst[fd][i] == '\n')
 		{
-			printf("%d\n", i);
+			//printf("%d\n", i);
 			line = ft_substr(fdlst[fd], 0, i + 1);
 			fdlst[fd] = (ft_strchr(fdlst[fd], '\n') + 1);
 			return (line);
@@ -48,11 +48,19 @@ char	*get_next_line(int fd)
 	char			*rbuf;//buffer to receive characters read
 	ssize_t			nrlet;//number of chars read
 
-	nrlet = read(fd, rbuf, BUFFER_SIZE);
+	rbuf = malloc(sizeof(char) * BUFFER_SIZE);
+	if (!rbuf)
+		return (0);
+	nrlet = read(fd, rbuf, (size_t )BUFFER_SIZE);
+	//printf("%s\n", keep[fd]);
 	if (nrlet || keep[fd])
 	{
-		res = makekeep(keep, rbuf, fd);
+		res = makekeep(keep, rbuf, fd, nrlet);
+		free(rbuf);
+		if (!res)
+			return (get_next_line(fd));
 		return (res);
 	}
+	free(rbuf);
 	return (0);
 }
--- test.c
+++ test.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leolivei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 14:59:09 by leolivei          #+#    #+#             */
+/*   Updated: 2021/11/29 16:16:40 by leolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdio.h>
 #include <sys/types.h>
@@ -6,14 +18,19 @@
 
 int	main(int argc, char **argv)
 {
-	int	fd1;
-	char	*s;
+	int		fd1;
+	int		i;
 
 	if (argc == 2)
 	{
 		fd1 = open(argv[1], O_RDONLY);
-		printf("%s\n", get_next_line(fd1));
-		printf("done");
+		i = 1;
+		while (i <= 6)
+		{
+			printf("linha %d\n", i);
+			printf("%s", get_next_line(fd1));
+			i++;
+		}
 		close(fd1);
 	}
 	return (0);
